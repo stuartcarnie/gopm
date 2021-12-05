fb53ae0af4e582903861def53a2a74736d502912
Roger Peppe <rogpeppe@gmail.com>
2021-12-05 17:07:46 +0000

    feat: use CUE for configuration
    We start to use CUE as the configuration language.
    We can potentially add back support for JSON and YAML
    configuration files later if we'd like (and support for a single
    file rather than a directory).

--- config/config.go
+++ config/config.go
@@ -1,109 +1,209 @@
 package config
 
 import (
+	_ "embed"
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
-	"gopkg.in/yaml.v3"
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/errors"
+	"cuelang.org/go/cue/load"
 )
 
-func Load(configFile string) (*Config, error) {
-	data, err := os.ReadFile(configFile)
+// Load loads the configuration at the given directory and returns it.
+// If root isn't empty, it configures the location of the filesystem root.
+func Load(configDir string, root string) (*Config, error) {
+	info, err := os.Stat(configDir)
 	if err != nil {
 		return nil, err
 	}
-	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
+	if !info.IsDir() {
+		// TODO allow YAML/JSON too?
+		return nil, fmt.Errorf("config %q must be a directory", configDir)
+	}
+	wd, err := os.Getwd()
+	if err != nil {
 		return nil, err
 	}
-	if cfg.Runtime.Root == "" {
-		cwd, err := os.Getwd()
-		if err != nil {
-			return nil, err
-		}
-		cfg.Runtime.Root = cwd
+	// runtime holds the values that are provided to the configuration
+	// from which everything else derives.
+	runtime := &RuntimeConfig{
+		Root:        root,
+		CWD:         wd,
+		Environment: make(map[string]string),
 	}
-	// TODO check for path conflicts in files.
+	for _, v := range os.Environ() {
+		vv := strings.SplitN(v, "=", 2)
+		runtime.Environment[vv[0]] = vv[1]
+	}
+
+	// Load the configuration, which will be incomplete initially
+	// as we haven't yet unified with the runtime config.
+	ctx := cuecontext.New()
+	insts := load.Instances([]string{"."}, &load.Config{
+		Dir: filepath.Join(wd, configDir),
+	})
+	vals, err := ctx.BuildInstances(insts)
+	if err != nil {
+		return nil, fmt.Errorf("cannot build instances: %v", err)
+	}
+	if len(vals) != 1 {
+		return nil, fmt.Errorf("wrong value count")
+	}
+
+	// Load the schema and defaults from our embedded CUE file (see schema.cue).
+	lschema, err := getLocalSchema(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get local schema: %v", err)
+	}
+
+	// Unify the user's configuration with the schema.
+	val := vals[0].Unify(lschema.config)
+
+	// Fill in the runtime config, which should complete everything.
+	val = val.FillPath(cue.ParsePath("runtime"), runtime)
+
+	// Check that it's all OK.
+	if err := val.Validate(cue.Concrete(true)); err != nil {
+		// TODO print to log file instead of directly to stderr.
+		errors.Print(os.Stderr, err, nil)
+		return nil, fmt.Errorf("config validation failed: %v", err)
+	}
+
+	// Export to JSON, then reimport, so we can apply our own defaults without
+	// conflicting with any defaults applied by the user's configuration.
+	data, err := val.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("cannot marshal: %v", err)
+	}
+
+	val = ctx.Encode(json.RawMessage(data))
+	if err := val.Err(); err != nil {
+		return nil, fmt.Errorf("cannot reencode configuration as CUE: %v", err)
+	}
+
+	// Apply our local defaults.
+	val = val.Unify(lschema.defaults)
+
+	// TODO check for path conflicts in files (or we could do that in the schema, I guess,
+	// although the error report would be more opaque).
+
+	// Decode into the final Config value.
+	var cfg Config
+	if err := val.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("cannot decode into config: %v", err)
+	}
 	return &cfg, nil
 }
 
+type localSchema struct {
+	// config holds the #Config definition
+	config cue.Value
+	// defaults holds the #Defaults definition.
+	defaults cue.Value
+}
+
+//go:embed schema.cue
+var schema []byte
+
+// getLocalSchema returns the schema info from schema.cue.
+func getLocalSchema(ctx *cue.Context) (*localSchema, error) {
+	insts := load.Instances([]string{"/schema.cue"}, &load.Config{
+		Overlay: map[string]load.Source{
+			"/schema.cue": load.FromBytes(schema),
+		},
+	})
+	vals, err := ctx.BuildInstances(insts)
+	if err != nil {
+		return nil, fmt.Errorf("cannot build instances for local schema: %v", err)
+	}
+	if len(vals) != 1 {
+		return nil, fmt.Errorf("expected 1 value, got %d", len(insts))
+	}
+	val := vals[0]
+	configDef := val.LookupPath(cue.MakePath(cue.Def("#Config")))
+	if err := configDef.Err(); err != nil {
+		return nil, fmt.Errorf("cannot get #Config: %v", err)
+	}
+	defaultsDef := val.LookupPath(cue.MakePath(cue.Def("#ConfigWithDefaults")))
+	if err := configDef.Err(); err != nil {
+		return nil, fmt.Errorf("cannot get #ConfigWithDefaults: %v", err)
+	}
+	return &localSchema{
+		config:   configDef,
+		defaults: defaultsDef,
+	}, nil
+}
+
+// Config defines the gopm configuration data.
 type Config struct {
-	Environment map[string]string   `yaml:"environment"`
-	HTTPServer  *Server             `yaml:"http_server"`
-	GRPCServer  *Server             `yaml:"grpc_server"`
-	Programs    map[string]*Program `yaml:"programs"`
-	FileSystem  map[string]*File    `yaml:"filesystem"`
-	Runtime     RuntimeConfig       `yaml:"runtime"`
+	// Runtime is provided to the user in order that they
+	// can fill out the rest of their configuration.
+	Runtime RuntimeConfig `json:"runtime"`
+
+	Environment map[string]string   `json:"environment"`
+	HTTPServer  *Server             `json:"http_server"`
+	GRPCServer  *Server             `json:"grpc_server"`
+	Programs    map[string]*Program `json:"programs"`
+	FileSystem  map[string]*File    `json:"filesystem"`
 }
 
 type File struct {
-	Name    string `yaml:"name"`
-	Path    string `yaml:"path"`
-	Content string `yaml:"content"`
-	// FullPath is filled in later
+	Name    string `json:"name"`
+	Path    string `json:"path"`
+	Content string `json:"content"`
 }
 
 type RuntimeConfig struct {
 	Environment map[string]string `json:"environment"`
-	Root        string            `json:"root"`
+	Root        string            `json:"root,omitempty"`
 	CWD         string            `json:"cwd"`
 }
 
 type Program struct {
-	Name                     string            `yaml:"name"`
-	Directory                string            `yaml:"directory"`
-	Command                  string            `yaml:"command"`
-	Environment              map[string]string `yaml:"environment"`
-	User                     string            `yaml:"user"`
-	ExitCodes                []int             `yaml:"exit_codes" default:"[0,2]"`
-	Priority                 int               `yaml:"priority" default:"999"`
-	RestartPause             Duration          `yaml:"restart_pause"`
-	StartRetries             int               `yaml:"start_retries" default:"3"`
-	StartSeconds             Duration          `yaml:"start_seconds" default:"\"1s\""`
-	Cron                     string            `yaml:"cron"`
-	AutoStart                bool              `yaml:"auto_start" default:"true"`
-	AutoRestart              *bool             `yaml:"auto_restart"`
-	RestartDirectoryMonitor  string            `yaml:"restart_directory_monitor"`
-	RestartFilePattern       string            `yaml:"restart_file_pattern" default:"*"`
-	RestartWhenBinaryChanged bool              `yaml:"restart_when_binary_changed"`
-	StopSignals              []string          `yaml:"stop_signals"`
-	StopWaitSeconds          Duration          `yaml:"stop_wait_seconds" default:"\"10s\""`
-	StopAsGroup              bool              `yaml:"stop_as_group"`
-	KillAsGroup              bool              `yaml:"kill_as_group"`
-	StdoutLogFile            string            `yaml:"stdout_logfile" default:"/dev/null"`
-	StdoutLogfileBackups     int               `yaml:"stdout_logfile_backups" default:"10"`
-	StdoutLogFileMaxBytes    int               `yaml:"stdout_logfile_max_bytes" default:"52428800"`
-	RedirectStderr           bool              `yaml:"redirect_stderr"`
-	StderrLogFile            string            `yaml:"stderr_logfile" default:"/dev/null"`
-	StderrLogfileBackups     int               `yaml:"stderr_logfile_backups" default:"10"`
-	StderrLogFileMaxBytes    int               `yaml:"stderr_logfile_max_bytes" default:"52428800"`
-	DependsOn                []string          `yaml:"depends_on"`
-	Labels                   map[string]string `yaml:"labels"`
+	Name                     string            `json:"name"`
+	Directory                string            `json:"directory"`
+	Command                  string            `json:"command"`
+	Environment              map[string]string `json:"environment"`
+	User                     string            `json:"user"`
+	ExitCodes                []int             `json:"exit_codes"`
+	Priority                 int               `json:"priority"`
+	RestartPause             Duration          `json:"restart_pause"`
+	StartRetries             int               `json:"start_retries"`
+	StartSeconds             Duration          `json:"start_seconds"`
+	Cron                     string            `json:"cron"`
+	AutoStart                bool              `json:"auto_start"`
+	AutoRestart              *bool             `json:"auto_restart,omitempty"`
+	RestartDirectoryMonitor  string            `json:"restart_directory_monitor"`
+	RestartFilePattern       string            `json:"restart_file_pattern"`
+	RestartWhenBinaryChanged bool              `json:"restart_when_binary_changed"`
+	StopSignals              []string          `json:"stop_signals"`
+	StopWaitSeconds          Duration          `json:"stop_wait_seconds"`
+	StopAsGroup              bool              `json:"stop_as_group"`
+	KillAsGroup              bool              `json:"kill_as_group"`
+	StdoutLogFile            string            `json:"stdout_logfile"`
+	StdoutLogfileBackups     int               `json:"stdout_logfile_backups"`
+	StdoutLogFileMaxBytes    int               `json:"stdout_logfile_max_bytes"`
+	RedirectStderr           bool              `json:"redirect_stderr"`
+	StderrLogFile            string            `json:"stderr_logfile"`
+	StderrLogfileBackups     int               `json:"stderr_logfile_backups"`
+	StderrLogFileMaxBytes    int               `json:"stderr_logfile_max_bytes"`
+	DependsOn                []string          `json:"depends_on"`
+	Labels                   map[string]string `json:"labels"`
 }
 
 type Server struct {
-	Address  string `yaml:"address"`
-	Network  string `yaml:"network"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
+	Address string `json:"address"`
+	Network string `json:"network"`
 }
 
 type Duration struct {
 	D time.Duration
-}
-
-func (d *Duration) UnmarshalYAML(value *yaml.Node) error {
-	var s string
-	if err := value.Decode(&s); err != nil {
-		return err
-	}
-	v, err := time.ParseDuration(s)
-	if err != nil {
-		return err
-	}
-	d.D = v
-	return nil
 }
 
 func (d *Duration) UnmarshalJSON(bytes []byte) error {
---
+++ config/schema.cue
@@ -0,0 +1,142 @@
+package gopm
+
+import (
+	"list"
+	pathpkg "path"
+	"time"
+)
+
+#Config: {
+	// runtime is filled in by gopm itself and can be used to
+	// fill in the rest of the configuration values.
+	runtime: #RuntimeConfig
+
+	// gprc_server specifies that a gRPC server should
+	// run providing access to the running gopm programs.
+	grpc_server?: #Server
+
+	// http_server specifies that an HTTP server should
+	// run providing access to the running gopm programs.
+	http_server?: #Server
+
+	// programs holds all the programs to start. Each program will only
+	// be started when its dependencies have successfully started.
+	programs: [string]: #Program
+	programs: [name=_]: "name": name
+
+	// filesystem holds the files to create before starting the programs.
+	// It's keyed by an arbitrary key (not necessarily the file path).
+	// The full filename of a given file f can be found by looking in
+	// #paths[f].
+	filesystem: [string]: #File
+	filesystem: [name=_]: "name": name
+
+	// #paths holds an entry for each file, making it easier for a configuration
+	// to find out the full path name of any file above.
+	#Paths: [string]: string
+	#Paths: {
+		for name, f in filesystem {
+			// The string interpolation below is to work around https://github.com/cue-lang/cue/issues/1409
+			"\(name)": pathpkg.Join(["\(runtime.root)", f.path], pathpkg.Unix)
+		}
+	}
+}
+
+#Server: {
+	address: string & =~ "."
+	network?: string
+}
+
+#RuntimeConfig: {
+	// env holds all the environment variables when gopm is started.
+	// TODO as an alternative, we could say that this should
+	// be filled in with incomplete entries specifying what env vars
+	// might be expected (which could also include constraints on their values)
+	environment: [string]: string
+
+	// root holds the root directory configured for gopm's filesystem.
+	root: *cwd | string
+
+	// cwd holds the current working directory.
+	cwd: string
+}
+
+#Program: {
+	name:       =~"^\\w+$"
+	directory?: string
+	command:    string & =~ "."
+	environment?: {
+		{[=~"^\\w+$"]: string}
+	}
+	user?: string
+	exit_codes?: [...int & >=0 & <=255]
+	priority?:      int
+	restart_pause?: int
+	start_retries?: int & >=0
+	start_seconds?: time.Duration
+	cron?:          string		// TODO validate this
+	auto_start?:    bool
+	auto_restart?:  bool
+
+	// Path to a directory to monitor and automatically restart the
+	// process if changes are detected
+	restart_directory_monitor?: string
+
+	// A pattern used to monitor a path and automatically restart the
+	// process if changes are detected
+	restart_file_pattern?: string
+
+	// Automatically restart the process if changes to the binary are
+	// detected
+	restart_when_binary_changed?: bool
+	stop_signals?:                list.UniqueItems() & [..."HUP" | "INT" | "QUIT" | "KILL" | "TERM" | "USR1" | "USR2"]
+
+	// The time to wait for the process to stop before killing.
+	stop_wait_seconds?:        time.Duration
+	stop_as_group?:            bool
+	kill_as_group?:            bool
+	stdout_logfile?:           string
+	stdout_logfile_backups?:   int
+	stdout_logfile_max_bytes?: int
+
+	// Redirect STDERR to STDOUT
+	redirect_stderr?:          bool
+	stderr_logfile?:           string
+	stderr_logfile_backups?:   int
+	stderr_logfile_max_bytes?: int
+
+	// A list of process names that must be started before starting
+	// this process
+	depends_on?: [...string]
+	labels: [string]: string
+}
+
+#File: {
+	name:    =~"^\\w+$"
+	path:    string & =~ "."
+	content: string
+}
+
+#ConfigWithDefaults: #Config &  {
+	...
+	programs: [_]: #Program & {
+		exit_codes: *[0, 2] | _
+		priority: *999 | _
+		start_retries: *3 | _
+		start_seconds: *"1s" | _
+		auto_start: *true | _
+		auto_restart: *false | _
+		restart_file_pattern: *"*" | _
+		kill_as_group: *true | _
+		stop_signals: *["INT"] | _
+		stop_as_group: *true | _
+		stop_wait_seconds: *"10s" | _
+		stdout_logfile: *"/dev/null" | _
+		stdout_logfile_backups: *10 | _
+		stdout_logfile_max_bytes: *50Mi | _
+		stderr_logfile: *"/dev/null" | _
+		stderr_logfile_backups: *10 | _
+		stderr_logfile_max_bytes: *50Mi | _
+		...
+	}
+}
